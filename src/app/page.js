a015ef402cb1b266f959fdef5612565d23470a2b
deonnabrammer <dmbram03@louisville.edu>
2025-02-18 19:12:43 -0500

    Changed styling from CSS to Tailwind
    Signed-off-by: deonnabrammer <dmbram03@louisville.edu>

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,9 @@ export default function Home() {
   const googleAPIKey = process.env.GOOGLE_API_KEY;
 
   return (
-    <div className="map">
+    <div className="h-dvh w-dvw">
       <iframe
-        width="450"
-        height="250"
-        frameBorder="0"
-        style={{ border: 0 }}
+        className="h-dvh w-dvw"
         referrerPolicy="no-referrer-when-downgrade"
         src={`https://www.google.com/maps/embed/v1/place?key=${googleAPIKey}&q=Louisville,KY`}
         allowFullScreen
