648379c4bcad60339e28899ea37eeb31475f4650
deonnabrammer <dmbram03@louisville.edu>
2025-03-28 11:22:46 -0400

    various changes, slider bar, layout, color, WIP

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { APIProvider, Map, useMap } from "@vis.gl/react-google-maps";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
 export default function Home() {
   const googleAPIKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
@@ -9,26 +11,35 @@ export default function Home() {
   const [toggleOverlay, setToggleOverlay] = useState(null);
 
   return (
-    <div className="h-screen w-screen bg-white">
-      <div className="flex flex-row justify-center items-center">
-        <OverlayToggleButton toggleOverlay={toggleOverlay} />
+    <div className="w-dvw h-dvh bg-zinc-800">
+      <div className="flex flex-wrap justify-center w-full bg-zinc-800">
+        <div className="p-10">
+          <OverlayToggleButton toggleOverlay={toggleOverlay} />
+        </div>
+        <div className="p-10">
+          <SliderBar />
+        </div>
       </div>
 
-      <APIProvider apiKey={googleAPIKey}>
-        <Map
-          defaultZoom={12}
-          defaultCenter={{ lat: 38.2469, lng: -85.7664 }}
-          onCameraChanged={(ev) =>
-            console.log(
-              "Camera changed:",
-              ev.detail.center,
-              "Zoom:",
-              ev.detail.zoom
-            )
-          }
-        />
-        <OverlayMap setToggleOverlay={setToggleOverlay} />
-      </APIProvider>
+      <div className="flex h-full w-full">
+        <div className="flex-grow w-full h-full bg-zinc-400 rounded-3xl overflow-hidden">
+          <APIProvider apiKey={googleAPIKey}>
+            <Map
+              defaultZoom={12}
+              defaultCenter={{ lat: 38.2469, lng: -85.7664 }}
+              onCameraChanged={(ev) =>
+                console.log(
+                  "Camera changed:",
+                  ev.detail.center,
+                  "Zoom:",
+                  ev.detail.zoom
+                )
+              }
+            />
+            <OverlayMap setToggleOverlay={setToggleOverlay} />
+          </APIProvider>
+        </div>
+      </div>
     </div>
   );
 }
@@ -75,5 +86,57 @@ function OverlayToggleButton({ toggleOverlay }) {
     >
       Toggle Overlay
     </button>
+  );
+}
+
+const marks = [
+  {
+    value: 0,
+    label: "1960",
+  },
+  {
+    value: 17,
+    label: "1970",
+  },
+  {
+    value: 34,
+    label: "1980",
+  },
+  {
+    value: 51,
+    label: "1990",
+  },
+  {
+    value: 68,
+    label: "2000",
+  },
+  {
+    value: 84,
+    label: "2010",
+  },
+  {
+    value: 100,
+    label: "2020",
+  },
+];
+
+function SliderBar() {
+  return (
+    <Box sx={{ width: 500 }}>
+      <Slider
+        aria-label="Custom Marks"
+        defaultValue={50}
+        getAriaValueText={(value) => "{value}"}
+        step={null}
+        valueLabelDisplay="off"
+        marks={marks}
+        color="secondary"
+        sx={{
+          "& .MuiSlider-markLabel": {
+            color: "white",
+          },
+        }}
+      />
+    </Box>
   );
 }
